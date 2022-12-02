e155a5d9ab3fb446c3654bd8b045fd5f2a5d808a
Marcos Cassiano <marcoscssno@gmail.com>
2022-12-01 22:46:55 -0300

    Add app version to AppBar

--- components/Layout.js
+++ components/Layout.js
@@ -11,17 +11,23 @@ import Link from '../src/Link'
 import moment from 'moment';
 // Other components
 import AuthenticationMenu from './AuthenticationMenu';
+// Next.js config
+import getConfig from 'next/config';
 
 export default function Layout(props) {
+    const { publicRuntimeConfig } = getConfig();
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Typography variant="h6" component="div">
                             <Link href="/" color="inherit" underline="none">
                                 Sivic
                             </Link>
+                        </Typography>
+                        <Typography variant="caption" sx={{ ml: 1, flexGrow: 1 }}>
+                            {publicRuntimeConfig?.version}
                         </Typography>
                         <Typography sx={{ flexGrow: 1 }}>
                             {moment().format("dddd, D [de] MMMM [de] YYYY")}
